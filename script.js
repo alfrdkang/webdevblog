32d55d40c1263e35db242cefb05303f7fe634ba4
Alfred-Kang <alfredkangjr@gmail.com>
2023-11-02 10:11:22 +0800

    fix: dark mode button

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function modeSwitch() {
     var element = document.body;
-    element.classList.toggle("dark-mode");
+    element.classList.toggle("light-mode");
+  }
+
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
   }
